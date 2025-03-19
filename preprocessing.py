fca0174d8baf27623ff5ea6b07b8b338479ed760
Dale Ren <daleren.y@gmail.com>
2025-03-18 17:49:46 -0700

    added feature (elo with consideration to point differential)

---
+++ preprocessing.py
@@ -0,0 +1,38 @@
+years = [year for year in range(2008, 2024 + 1)]
+
+import pandas as pd
+import numpy as np
+
+for i, year in enumerate(years):
+    games_df = pd.read_csv(f'game-data/{year}games.csv', index_col='Unnamed: 0')
+
+    def parse_result(result):
+        result_parts = result.split(', ')
+        outcome = result_parts[0]  # W or L
+        scores = list(map(int, result_parts[1].split('-')))
+        
+        if outcome == 'W':
+            team_points = max(scores)
+            opponent_points = min(scores)
+        else:
+            team_points = min(scores)
+            opponent_points = max(scores)
+        
+        return pd.Series([outcome, team_points, opponent_points])
+    
+    games_df[['Result', 'Team Points', 'Opponent Points']] = games_df['Result'].apply(parse_result)
+    games_df['Point Differential'] = games_df['Team Points'] - games_df['Opponent Points']
+
+    games_df['Date'] = pd.to_datetime(games_df['Date'])
+    games_df = games_df.sort_values('Date')
+
+    # Columns to drop:
+    #   Rank, Game ID, Season Year, Game Breakdown, Extra
+    games_df.drop(columns = ['Rank', 'Game ID', 'Season Year', 'Game Breakdown', 'Extra'])
+
+    try:
+        games_df.to_csv(f'game-data/{year}games.csv')    
+        print(f'{i + 1}/{len(years)} | successfully preprocessed games from {year}')
+    except Exception as e:
+        print('ERROR:', e)
+
--- scraping.py
+++ scraping.py
@@ -11,13 +11,17 @@ columns = [
     "Opponent Coach", "Unknown", "Game Importance", "Game Breakdown", "Extra"
 ]
 
-for year in years:
+for i, year in enumerate(years):
     url = f'https://barttorvik.com/getgamestats.php?sIndex=7&year={year}&tvalue=All&cvalue=All&opcvalue=All&ovalue=All&minwin=All&mindate=&maxdate=&typev=All&venvalue=All&minadjo=0&minadjd=200&mintempo=0&minppp=0&minefg=0&mintov=200&minreb=0&minftr=0&minpppd=200&minefgd=200&mintovd=0&minrebd=200&minftrd=200&mings=0&mingscript=-100&maxx=100&coach=All&opcoach=All&adjoSelect=min&adjdSelect=max&tempoSelect=min&pppSelect=min&efgSelect=min&tovSelect=max&rebSelect=min&ftrSelect=min&pppdSelect=max&efgdSelect=max&tovdSelect=min&rebdSelect=max&ftrdSelect=max&gscriptSelect=min&sortToggle=1'
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data, columns=columns)
-        df.to_csv(f'game-data/{year}games.csv')
+        try:
+            df.to_csv(f'game-data/{year}games.csv')
+            print(f'{i + 1}/{len(years)} | successfullsy scraped games from {year}')
+        except Exception as e:
+            print('ERROR:', e)
     else:
         print(f"Error: {response.status_code}, {response.text}")
